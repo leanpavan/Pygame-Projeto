f4cd369a6826cae0a7193fbc402b2c7e1d9716ed
Leandro Pavan <lejpavan17@gmail.com>
2024-05-23 11:09:23 -0300

    spritess

--- main.py
+++ main.py
@@ -4,29 +4,54 @@ import pygame
 # Pygame setup
 pygame.init()
 
+# Screen
 WIDTH = 800
 HEIGHT = 600
 
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Cogumelos X Fungos")
+
+# Framerate
 clock = pygame.time.Clock()
+FPS = 30
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-x = 0
+class Square(pygame.sprite.Sprite):
+    def __init__(self,col,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50,50))
+        self.image.fill(col)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+
+square = Square("crimson", 500,300)
+
+# Create sprite group for squares
+squares = pygame.sprite.Group()
+squares.add(square)
+
 
 # Main game loop
 running = True
 while running:
+    # Quit the game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    # Clock
+    dt = clock.tick(FPS) / 1000
+    # Update background
     screen.fill('black')
+    # Update sprite group
+    squares.update()
+    # Draw sprite group
+    squares.draw(screen)
 
 
-    pygame.draw.rect(screen,(100,200,80),(x,300,60,20))
+    pygame.draw.rect(screen,(100,200,80),(100,300,60,20))
     pygame.draw.circle(screen, "red", player_pos, 40)
-    x += 1
 
     # Character Movement
     keys = pygame.key.get_pressed()
@@ -40,7 +65,8 @@ while running:
         player_pos.x += 300 * dt
 
     pygame.display.update()
-    dt = clock.tick(30) / 1000
+
+
 
 # Quit
 pygame.quit()
