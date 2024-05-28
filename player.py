fa53a6f2caae02bcb638ed85c49bbdbca2b0e3c5
Lucas Aioria Serpa <Lucasaioriaserpa@outlook.com>
2024-05-28 09:14:14 -0300

    otimização e criação de classes e biblioteca animation.py
    Sistema de movimento foi movido para a classe player
    
    Quase todo sistema de animação foi movido para animations.py !!
    
    :ant: :ant: :ant: :ant:

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 #Imports
 import pygame
-import spritesheet as ss
+import animations as an_
+import spritesheet as ss_
 
 class player():
     def __init__(self, player_pos, player_sprite):
@@ -9,5 +10,16 @@ class player():
         self.player_hp = 100
         self.player_walkspeed = 300
 
-    def walk_animation(self):
-        return
+    def movement(self,keys:list,dt:float):
+        '''Keys: pygame.key.get_pressed()
+        dt: clock game'''
+        if keys[pygame.K_a]:
+            self.player_pos.x -= self.player_walkspeed * dt
+            an_.animation.action = 1
+        elif keys[pygame.K_d]:
+            self.player_pos.x += self.player_walkspeed * dt
+            an_.animation.action = 1
+        elif keys[pygame.K_a] == False and keys[pygame.K_d] == False:
+            if an_.animation.frame >= 4:
+                an_.animation.frame = 0
+            an_.animation.action = 0
--- spritesheets.py
+++ spritesheets.py
@@ -1,8 +1,10 @@
-# Imports
+# Imports librarys
 import pygame
-import player
-import spritesheet
 import tkinter as tk
+
+# Imports self.librarys
+import spritesheet as ss_
+import player as pl_
 
 
 # Pygame setup
@@ -25,28 +27,16 @@ pygame.display.set_caption("Spritesheets")
 BG = "#aaaaaa"
 BLACK = "#000000"
 
-# Create animation list
-animation_list = []
-animation_steps = [4, 6, 3, 4]
-action = 0
-last_update = pygame.time.get_ticks()
-animation_cooldow = 150
-frame = 0
-step_counter = 0
-
-for animation in animation_steps:
-    temp_img_list = []
-    for _ in range(animation):
-        temp_img_list.append(spritesheet.sprite_sheet().get_image(step_counter, 24,24, 3))
-        step_counter += 1
-    animation_list.append(temp_img_list)
+# Create animation object
+import animations as an_
+an_.animation.create_animation_list()
 
 # Framerate
 clock = pygame.time.Clock()
 FPS = 30
 
 # Player
-player = player.player(pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2), spritesheet.sprite_sheet())
+player = pl_.player(pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2), ss_.sprite_sheet())
 
 # Game Loop
 running = True
@@ -55,39 +45,29 @@ while running:
     # Update background
     screen.fill(BG)
 
+
     # Clock
     dt = clock.tick(FPS) / 1000
 
+
     # Update animation
     current_time = pygame.time.get_ticks()
-    if current_time - last_update >= animation_cooldow:
-        frame += 1
-        last_update = current_time
-        if frame >= len(animation_list[action]):
-            frame = 0
+    an_.animation.update_animation(current_time)
 
 
     # Show frame image
-    screen.blit(animation_list[action][frame], (player.player_pos,player.player_pos))
+    screen.blit(an_.animation.animation_list[an_.animation.action][an_.animation.frame], (player.player_pos,player.player_pos))
 
 
     # Player movement
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:
-        player.player_pos.x -= player.player_walkspeed * dt
-        action = 1
-    elif keys[pygame.K_d]:
-        player.player_pos.x += player.player_walkspeed * dt
-        action = 1
-    elif keys[pygame.K_a] == False and keys[pygame.K_d] == False:
-        if frame >= 4:
-            frame = 0
-        action = 0
+    player.movement(keys,dt)
 
 
     # Event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
             running = False
+
 
     pygame.display.update()
