4f7d26b98a30ac2988710578e16448cfbd9611dd
Leandro Pavan <lejpavan17@gmail.com>
2024-06-10 11:22:37 -0300

    sprite imports and player movement

--- level.py
+++ level.py
@@ -1,4 +1,8 @@
 import pygame
+from settings import *
+from tile import Tile
+from player import Player
+from debug import debug
 
 class Level:
     def __init__(self):
@@ -8,8 +12,23 @@ class Level:
 
         #Sprite group setup
         self.visible_sprites = pygame.sprite.Group()
-        self.obstacles_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
+
+        #sprite setup
+        self.create_map()
+
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILEZISE
+                y = row_index * TILEZISE
+                if col == 'x':
+                    Tile((x,y), [self.visible_sprites, self.obstacle_sprites])
+                if col == 'p':
+                    self.player = Player((x,y), [self.visible_sprites])
 
     def run(self):
         #update and draw the game
-        pass
+        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
